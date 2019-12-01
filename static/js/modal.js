cb2420b60b1fb556aa15ca3b48f4cc02d84f1cb9
jfaldanam <jfaldanam@gmail.com>
2019-12-01 18:43:01 +0100

    Added comment list for buses TODO: css

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,6 +1,7 @@
 // Get the modal and close modal elements
 var modal = document.getElementById("infoEMT");
 var span = document.getElementsByClassName("close")[0];
+const urlBusComments = 'http://localhost:5001/api/v1/comments/EMTCode/'
 
 // Opens the modal, trigered when a bus marker is clicked
 function showModal(element) {
@@ -10,6 +11,8 @@ function showModal(element) {
     document.getElementById("modalLineCode").innerText = "Line: " + properties.codLine;
     document.getElementById("modalDirection").innerText = "Direction: " + properties.direction;
     document.getElementById("modalLastUpdate").innerText = "Last update: " + properties.lastUpdate;
+    clearRows();
+    showCommentList('b' + properties.busCode);
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -22,4 +25,45 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+// Comments
+
+// Clears the comments table
+function clearRows() {
+  var table = document.getElementById("commentsTable");
+  for(var i = table.rows.length - 1; i > 0; i--) {
+      table.deleteRow(i);
+  }
+}
+
+// Adds a new data row to the comments table
+function addRow(content) {
+  tabBody = document.getElementById("commentsTable");
+  row = document.createElement("tr");
+  cell = document.createElement("td");
+  cell.appendChild(content);
+  row.appendChild(cell);
+  tabBody.appendChild(row);
+}
+
+// Add all comments for a bus to the comments table
+function showCommentList(busCode) {
+  httpGetAsync(urlBusComments + busCode, function(response) {   
+    console.log('Updating buses position');
+    response = JSON.parse(response);
+    console.log(response[0])
+    for (var i = 0; i<response.length; i++) {
+      var author = response[i]['userId']
+      var body = response[i]['body']
+      var image = response[i]['imageURL']
+
+      var div = document.createElement('div')
+      div.innerHTML = author + '</br>' + body + '</br><img src="' + image + '"/>'
+
+      addRow(div)
+    }
+}, function(response) {
+    console.log('Error on load comments = ' + response);
+});
 }
