e25b931f3e3b834d2674c896dd256972623b50b3
jotapax <javi98perez17@gmail.com>
2019-12-21 17:05:16 +0100

    dark mode scrollbar

--- static/js/map.js
+++ static/js/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
         maxZoom: 19,
-        minZoom: 2
+        minZoom: 12
     }).addTo(map);
 
 // Show stops and start refresh cycle for the buses
